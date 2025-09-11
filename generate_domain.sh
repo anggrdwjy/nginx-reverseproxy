c5c237cd7b03e9e2fcaf1aa14d0cd3a7e1caf990
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-11 18:29:51 +0700

    Update generate_domain.sh

--- generate_domain.sh
+++ generate_domain.sh
@@ -6,17 +6,20 @@ echo "   ==============================================   ";
 echo "              Generate Domain with SSL              ";
 echo "   ==============================================   ";
 echo "                                                    ";
-echo -n "   Custom DOMAIN : "
+echo -n "   Add DOMAIN    : "
 read DOMAIN
-echo -n "   Custom POINTING IP : "
+echo -n "   HTTP / HTTPS  : "
+read HTTP
+echo -n "   POINTING IP   : "
 read POINTING_IP
-echo -n "   Custom POINTING PORT : "
+echo -n "   POINTING PORT : "
 read POINTING_PORT
 echo "                                                    ";
 echo "   ==============================================   ";
 echo "                                                    ";
 sudo cp support/domain /etc/nginx/sites-available/$DOMAIN
 sed -i "s/domain/$DOMAIN/g" /etc/nginx/sites-available/$DOMAIN
+sed -i "s/http/$HTTP/g" /etc/nginx/sites-available/$DOMAIN
 sed -i "s/localhost/$POINTING_IP/g" /etc/nginx/sites-available/$DOMAIN
 sed -i "s/port/$POINTING_PORT/g" /etc/nginx/sites-available/$DOMAIN
 sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
