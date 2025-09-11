a63bff6544baddc58f8323a1c300947ed3200537
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-11 18:30:49 +0700

    Update install-nginx-ssl.sh

--- install-nginx-ssl.sh
+++ install-nginx-ssl.sh
@@ -2,7 +2,7 @@
 #By Anggarda Saputra Wijaya
 #Install NGINX & SSL
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt update && sudo apt install nginx certbot python3-certbot-nginx -y
+sudo apt install nginx certbot python3-certbot-nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
 ufw allow 443
